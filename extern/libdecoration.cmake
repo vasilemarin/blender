464c242deb6e4b70041f06482790a952c80208e9
Christian Rauch <Rauch.Christian@gmx.de>
2020-05-21 21:54:54 +0100

    branch parent_frame

--- extern/libdecoration.cmake
+++ extern/libdecoration.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject)
 ExternalProject_Add(libdecoration
-    GIT_REPOSITORY https://gitlab.gnome.org/jadahl/libdecoration.git
-    GIT_TAG master
+    GIT_REPOSITORY https://gitlab.gnome.org/christian-rauch/libdecoration.git
+    GIT_TAG parent_frame
     PREFIX "${CMAKE_CURRENT_BINARY_DIR}/libdecoration"
     CONFIGURE_COMMAND meson --prefix "${CMAKE_BINARY_DIR}" --libdir "lib" ../libdecoration
     BUILD_COMMAND ninja
