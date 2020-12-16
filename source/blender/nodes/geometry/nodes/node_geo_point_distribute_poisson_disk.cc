vasilemarin/blender

commit a7628ec22abca8d1aaada75a8227638947168a3a

Geometry Nodes: Poisson disk point distribution node/method
This patch does two things:
* Introduce a Seed to the random distribution method
* Bring in a new distribution method for the point scattering node

Patch Review: https://developer.blender.org/D9787

Note: This commit doesn't not handle doversion. Which means that users
need to manually update their files that were using the Point Distribute
node and reconnect inputs to the "Maximum Density" socket.

Original patch by Sebastian Parborg, with changes to not rely on the cy
libraries and overall cleanup.

Patch review by Jacques Lucke, besides help with the new "heap" system
that was required for this algorithm.

Based on Cem Yuksel. 2015. Sample Elimination for Generating Poisson Disk
Sample. Sets. Computer Graphics Forum 34, 2 (May 2015), 25-32
http://www.cemyuksel.com/research/sampleelimination/

source/blender/nodes/geometry/nodes/node_geo_point_distribute_poisson_disk.cc
@@ -1,0 +1,280 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+/*
+ * Based on Cem Yuksel. 2015. Sample Elimination for Generating Poisson Disk Sample
+ * ! Sets. Computer Graphics Forum 34, 2 (May 2015), 25-32.
+ * ! http://www.cemyuksel.com/research/sampleelimination/
+ * Copyright (c) 2016, Cem Yuksel <cem@cemyuksel.com>
+ * All rights reserved.
+ */
+
+#include "BLI_inplace_priority_queue.hh"
+#include "BLI_kdtree.h"
+
+#include "node_geometry_util.hh"
+
+#include <iostream>
+
+namespace blender::nodes {
+
+static void tile_point(Vector<float3> *tiled_points,
+                       Vector<size_t> *indices,
+                       const float maximum_distance,
+                       const float3 boundbox,
+                       float3 const &point,
+                       size_t index,
+                       int dimension = 0)
+{
+  for (int dimension_iter = dimension; dimension_iter < 3; dimension_iter++) {
+    if (boundbox[dimension_iter] - point[dimension_iter] < maximum_distance) {
+      float3 point_tiled = point;
+      point_tiled[dimension_iter] -= boundbox[dimension_iter];
+
+      tiled_points->append(point_tiled);
+      indices->append(index);
+
+      tile_point(tiled_points,
+                 indices,
+                 maximum_distance,
+                 boundbox,
+                 point_tiled,
+                 index,
+                 dimension_iter + 1);
+    }
+
+    if (point[dimension_iter] < maximum_distance) {
+      float3 point_tiled = point;
+      point_tiled[dimension_iter] += boundbox[dimension_iter];
+
+      tiled_points->append(point_tiled);
+      indices->append(index);
+
+      tile_point(tiled_points,
+                 indices,
+                 maximum_distance,
+                 boundbox,
+                 point_tiled,
+                 index,
+                 dimension_iter + 1);
+    }
+  }
+}
+
+/**
+ * Returns the weight the point gets based on the distance to another point.
+ */
+static float point_weight_influence_get(const float maximum_distance,
+                                        const float minimum_distance,
+                                        float distance)
+{
+  const float alpha = 8.0f;
+
+  if (distance < minimum_distance) {
+    distance = minimum_distance;
+  }
+
+  return std::pow(1.0f - distance / maximum_distance, alpha);
+}
+
+/**
+ * Weight each point based on their proximity to its neighbors
+ *
+ * For each index in the weight array add a weight based on the proximity the
+ * corresponding point has with its neighboors.
+ **/
+static void points_distance_weight_calculate(Vector<float> *weights,
+                                             const size_t point_id,
+                                             const float3 *input_points,
+                                             const void *kd_tree,
+                                             const float minimum_distance,
+                                             const float maximum_distance,
+                                             InplacePriorityQueue<float> *heap)
+{
+  KDTreeNearest_3d *nearest_point = nullptr;
+  int neighbors = BLI_kdtree_3d_range_search(
+      (KDTree_3d *)kd_tree, input_points[point_id], &nearest_point, maximum_distance);
+
+  for (int i = 0; i < neighbors; i++) {
+    size_t neighbor_point_id = nearest_point[i].index;
+
+    if (neighbor_point_id >= weights->size()) {
+      continue;
+    }
+
+    /* The point should not influence itself. */
+    if (neighbor_point_id == point_id) {
+      continue;
+    }
+
+    const float weight_influence = point_weight_influence_get(
+        maximum_distance, minimum_distance, nearest_point[i].dist);
+
+    /* In the first pass we just the weights. */
+    if (heap == nullptr) {
+      (*weights)[point_id] += weight_influence;
+    }
+    /* When we run again we need to update the weights and the heap. */
+    else {
+      (*weights)[neighbor_point_id] -= weight_influence;
+      heap->priority_decreased(neighbor_point_id);
+    }
+  }
+
+  if (nearest_point) {
+    MEM_freeN(nearest_point);
+  }
+}
+
+/**
+ * Returns the minimum radius fraction used by the default weight function.
+ */
+static float weight_limit_fraction_get(const size_t input_size, const size_t output_size)
+{
+  const float beta = 0.65f;
+  const float gamma = 1.5f;
+  float ratio = float(output_size) / float(input_size);
+  return (1.0f - std::pow(ratio, gamma)) * beta;
+}
+
+/**
+ * Tile the input points.
+ */
+static void points_tiling(const float3 *input_points,
+                          const size_t input_size,
+                          void **kd_tree,
+                          const float maximum_distance,
+                          const float3 boundbox)
+
+{
+  Vector<float3> tiled_points(input_points, input_points + input_size);
+  Vector<size_t> indices(input_size);
+
+  for (size_t i = 0; i < input_size; i++) {
+    indices[i] = i;
+  }
+
+  /* Tile the tree based on the boundbox. */
+  for (size_t i = 0; i < input_size; i++) {
+    tile_point(&tiled_points, &indices, maximum_distance, boundbox, input_points[i], i);
+  }
+
+  /* Build a new tree with the new indices and tiled points. */
+  *kd_tree = BLI_kdtree_3d_new(tiled_points.size());
+  for (size_t i = 0; i < tiled_points.size(); i++) {
+    BLI_kdtree_3d_insert(*(KDTree_3d **)kd_tree, indices[i], tiled_points[i]);
+  }
+  BLI_kdtree_3d_balance(*(KDTree_3d **)kd_tree);
+}
+
+static void weighted_sample_elimination(const float3 *input_points,
+                                        const size_t input_size,
+                                        float3 *output_points,
+                                        const size_t output_size,
+                                        const float maximum_distance,
+                                        const float3 boundbox,
+                                        const bool do_copy_eliminated)
+{
+  const float minimum_distance = maximum_distance *
+                                 weight_limit_fraction_get(input_size, output_size);
+
+  void *kd_tree = nullptr;
+  points_tiling(input_points, input_size, &kd_tree, maximum_distance, boundbox);
+
+  /* Assign weights to each sample. */
+  Vector<float> weights(input_size, 0.0f);
+  for (size_t point_id = 0; point_id < weights.size(); point_id++) {
+    points_distance_weight_calculate(
+        &weights, point_id, input_points, kd_tree, minimum_distance, maximum_distance, nullptr);
+  }
+
+  /* Remove the points based on their weight. */
+  InplacePriorityQueue<float> heap(weights);
+
+  size_t sample_size = input_size;
+  while (sample_size > output_size) {
+    /* For each sample around it, remove its weight contribution and update the heap. */
+    size_t point_id = heap.pop_index();
+    points_distance_weight_calculate(
+        &weights, point_id, input_points, kd_tree, minimum_distance, maximum_distance, &heap);
+    sample_size--;
+  }
+
+  /* Copy the samples to the output array. */
+  size_t target_size = do_copy_eliminated ? input_size : output_size;
+  for (size_t i = 0; i < target_size; i++) {
+    size_t index = heap.all_indices()[i];
+    output_points[i] = input_points[index];
+  }
+
+  /* Cleanup. */
+  BLI_kdtree_3d_free((KDTree_3d *)kd_tree);
+}
+
+static void progressive_sampling_reorder(Vector<float3> *output_points,
+                                         float maximum_density,
+                                         float3 boundbox)
+{
+  /* Re-order the points for progressive sampling. */
+  Vector<float3> temporary_points(output_points->size());
+  float3 *source_points = output_points->data();
+  float3 *dest_points = temporary_points.data();
+  size_t source_size = output_points->size();
+  size_t dest_size = 0;
+
+  while (source_size >= 3) {
+    dest_size = source_size * 0.5f;
+
+    /* Changes the weight function radius using half of the number of samples.
+     * It is used for progressive sampling. */
+    maximum_density *= std::sqrt(2.0f);
+    weighted_sample_elimination(
+        source_points, source_size, dest_points, dest_size, maximum_density, boundbox, true);
+
+    if (dest_points != output_points->data()) {
+      mempcpy((*output_points)[dest_size],
+              dest_points[dest_size],
+              (source_size - dest_size) * sizeof(float3));
+    }
+
+    /* Swap the arrays around. */
+    float3 *points_iter = source_points;
+    source_points = dest_points;
+    dest_points = points_iter;
+    source_size = dest_size;
+  }
+  if (source_points != output_points->data()) {
+    memcpy(output_points->data(), source_points, dest_size * sizeof(float3));
+  }
+}
+
+void poisson_disk_point_elimination(Vector<float3> const *input_points,
+                                    Vector<float3> *output_points,
+                                    float maximum_density,
+                                    float3 boundbox)
+{
+  weighted_sample_elimination(input_points->data(),
+                              input_points->size(),
+                              output_points->data(),
+                              output_points->size(),
+                              maximum_density,
+                              boundbox,
+                              false);
+
+  progressive_sampling_reorder(output_points, maximum_density, boundbox);
+}
+
+}  // namespace blender::nodes

source/blender/blenlib/BLI_kdopbvh.h
@@ -178,6 +178,7 @@ int *BLI_bvhtree_intersect_plane(BVHTree *tree, float plane[4], uint *r_intersec
 int BLI_bvhtree_get_len(const BVHTree *tree);
 int BLI_bvhtree_get_tree_type(const BVHTree *tree);
 float BLI_bvhtree_get_epsilon(const BVHTree *tree);
+void BLI_bvhtree_get_bounding_box(BVHTree *tree, float r_bb_min[3], float r_bb_max[3]);
 
 /* find nearest node to the given coordinates
  * (if nearest is given it will only search nodes where

source/blender/blenlib/intern/BLI_kdopbvh.c
@@ -1076,6 +1076,25 @@ float BLI_bvhtree_get_epsilon(const BVHTree *tree)
   return tree->epsilon;
 }
 
+/**
+ * This function returns the bounding box of the BVH tree.
+ */
+void BLI_bvhtree_get_bounding_box(BVHTree *tree, float r_bb_min[3], float r_bb_max[3])
+{
+  BVHNode *root = tree->nodes[tree->totleaf];
+  if (root != NULL) {
+    const float bb_min[3] = {root->bv[0], root->bv[2], root->bv[4]};
+    const float bb_max[3] = {root->bv[1], root->bv[3], root->bv[5]};
+    copy_v3_v3(r_bb_min, bb_min);
+    copy_v3_v3(r_bb_max, bb_max);
+  }
+  else {
+    BLI_assert(false);
+    zero_v3(r_bb_min);
+    zero_v3(r_bb_max);
+  }
+}
+
 /** \} */
 
 /* -------------------------------------------------------------------- */

source/blender/editors/space_node/drawnode.c
@@ -3208,6 +3208,13 @@ static void node_geometry_buts_attribute_mix(uiLayout *layout,
   uiItemR(col, ptr, "input_type_b", DEFAULT_FLAGS, IFACE_("B"), ICON_NONE);
 }
 
+static void node_geometry_buts_attribute_point_distribute(uiLayout *layout,
+                                                          bContext *UNUSED(C),
+                                                          PointerRNA *ptr)
+{
+  uiItemR(layout, ptr, "distribute_method", DEFAULT_FLAGS, "", ICON_NONE);
+}
+
 static void node_geometry_set_butfunc(bNodeType *ntype)
 {
   switch (ntype->type) {
@@ -3234,6 +3241,9 @@ static void node_geometry_set_butfunc(bNodeType *ntype)
       break;
     case GEO_NODE_ATTRIBUTE_MIX:
       ntype->draw_buttons = node_geometry_buts_attribute_mix;
+      break;
+    case GEO_NODE_POINT_DISTRIBUTE:
+      ntype->draw_buttons = node_geometry_buts_attribute_point_distribute;
       break;
   }
 }

source/blender/makesdna/DNA_node_types.h
@@ -1501,6 +1501,11 @@ typedef enum GeometryNodeAttributeInputMode {
   GEO_NODE_ATTRIBUTE_INPUT_COLOR = 3,
 } GeometryNodeAttributeInputMode;
 
+typedef enum GeometryNodePointDistributeMethod {
+  GEO_NODE_POINT_DISTRIBUTE_RANDOM = 0,
+  GEO_NODE_POINT_DISTRIBUTE_POISSON = 1,
+} GeometryNodePointDistributeMethod;
+
 #ifdef __cplusplus
 }
 #endif

source/blender/makesrna/intern/rna_nodetree.c
@@ -452,6 +452,20 @@ static const EnumPropertyItem rna_node_geometry_attribute_input_type_items[] = {
     {0, NULL, 0, NULL, NULL},
 };
 
+static const EnumPropertyItem rna_node_geometry_point_distribute_method_items[] = {
+    {GEO_NODE_POINT_DISTRIBUTE_RANDOM,
+     "RANDOM",
+     0,
+     "Random",
+     "Distribute points randomly on the surface"},
+    {GEO_NODE_POINT_DISTRIBUTE_POISSON,
+     "POISSON",
+     0,
+     "Poisson Disk",
+     "Project points on the surface evenly with a Poisson disk distribution"},
+    {0, NULL, 0, NULL, NULL},
+};
+
 #endif
 
 #ifdef RNA_RUNTIME
@@ -8478,6 +8492,18 @@ static void def_geo_attribute_mix(StructRNA *srna)
   prop = RNA_def_property(srna, "input_type_b", PROP_ENUM, PROP_NONE);
   RNA_def_property_enum_items(prop, rna_node_geometry_attribute_input_type_items);
   RNA_def_property_ui_text(prop, "Input Type B", "");
+  RNA_def_property_update(prop, NC_NODE | NA_EDITED, "rna_Node_socket_update");
+}
+
+static void def_geo_point_distribute(StructRNA *srna)
+{
+  PropertyRNA *prop;
+
+  prop = RNA_def_property(srna, "distribute_method", PROP_ENUM, PROP_NONE);
+  RNA_def_property_enum_sdna(prop, NULL, "custom1");
+  RNA_def_property_enum_items(prop, rna_node_geometry_point_distribute_method_items);
+  RNA_def_property_enum_default(prop, GEO_NODE_POINT_DISTRIBUTE_RANDOM);
+  RNA_def_property_ui_text(prop, "Distribution Method", "Method to use for scattering points");
   RNA_def_property_update(prop, NC_NODE | NA_EDITED, "rna_Node_socket_update");
 }
 

source/blender/nodes/CMakeLists.txt
@@ -148,6 +148,7 @@ set(SRC
   geometry/nodes/node_geo_attribute_mix.cc
   geometry/nodes/node_geo_object_info.cc
   geometry/nodes/node_geo_point_distribute.cc
+  geometry/nodes/node_geo_point_distribute_poisson_disk.cc
   geometry/nodes/node_geo_point_instance.cc
   geometry/nodes/node_geo_subdivision_surface.cc
   geometry/nodes/node_geo_transform.cc

source/blender/nodes/NOD_static_types.h
@@ -271,7 +271,7 @@ DefNode(GeometryNode, GEO_NODE_EDGE_SPLIT, 0, "EDGE_SPLIT", EdgeSplit, "Edge Spl
 DefNode(GeometryNode, GEO_NODE_TRANSFORM, 0, "TRANSFORM", Transform, "Transform", "")
 DefNode(GeometryNode, GEO_NODE_SUBDIVISION_SURFACE, 0, "SUBDIVISION_SURFACE", SubdivisionSurface, "Subdivision Surface", "")
 DefNode(GeometryNode, GEO_NODE_BOOLEAN, def_geo_boolean, "BOOLEAN", Boolean, "Boolean", "")
-DefNode(GeometryNode, GEO_NODE_POINT_DISTRIBUTE, 0, "POINT_DISTRIBUTE", PointDistribute, "Point Distribute", "")
+DefNode(GeometryNode, GEO_NODE_POINT_DISTRIBUTE, def_geo_point_distribute, "POINT_DISTRIBUTE", PointDistribute, "Point Distribute", "")
 DefNode(GeometryNode, GEO_NODE_POINT_INSTANCE, def_geo_point_instance, "POINT_INSTANCE", PointInstance, "Point Instance", "")
 DefNode(GeometryNode, GEO_NODE_OBJECT_INFO, 0, "OBJECT_INFO", ObjectInfo, "Object Info", "")
 DefNode(GeometryNode, GEO_NODE_ATTRIBUTE_RANDOMIZE, def_geo_attribute_randomize, "ATTRIBUTE_RANDOMIZE", AttributeRandomize, "Attribute Randomize", "")

commit f19f511a1939ddb7f305b2db6597bdd5fa91da76

Cleanup: Replace mempcpy with memcpy
There is no need of using mempcpy here, memcpy is enough.

Note: This may fix building in Windows as well, which was broken since
a7628ec22a.

source/blender/nodes/geometry/nodes/node_geo_point_distribute_poisson_disk.cc
@@ -245,9 +245,9 @@ static void progressive_sampling_reorder(Vector<float3> *output_points,
         source_points, source_size, dest_points, dest_size, maximum_density, boundbox, true);
 
     if (dest_points != output_points->data()) {
-      mempcpy((*output_points)[dest_size],
-              dest_points[dest_size],
-              (source_size - dest_size) * sizeof(float3));
+      memcpy((*output_points)[dest_size],
+             dest_points[dest_size],
+             (source_size - dest_size) * sizeof(float3));
     }
 
     /* Swap the arrays around. */