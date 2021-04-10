6a1bf8dd13d7b38f018b50a3ea81700f6674f3db
makowalski <makowalski@nvidia.com>
2021-04-09 21:31:16 -0400

    USD Import: fixed build errors.

--- source/blender/io/usd/intern/usd_writer_material.cc
+++ source/blender/io/usd/intern/usd_writer_material.cc
@@ -59,7 +59,6 @@ extern "C" {
 
 #include "MEM_guardedalloc.h"
 #include "RNA_access.h"
-#include "RNA_blender_cpp.h"
 #include "RNA_types.h"
 
 #include <algorithm>
@@ -1373,18 +1372,10 @@ pxr::UsdShadeShader create_cycles_shader_node(pxr::UsdStageRefPtr a_stage,
                                shader,
                                (int)ima->alpha_mode);
 
-        // Colorspace RNA
-        PointerRNA id_ptr;
-        RNA_id_pointer_create(node->id, &id_ptr);
-        BL::Image b_image(id_ptr);
-        PointerRNA colorspace_ptr = b_image.colorspace_settings().ptr;
-        PropertyRNA *prop = RNA_struct_find_property(&colorspace_ptr, "name");
-        const char *identifier = "";
-        int value = RNA_property_enum_get(&colorspace_ptr, prop);
-        RNA_property_enum_identifier(NULL, &colorspace_ptr, prop, value, &identifier);
-
-        shader.CreateInput(cyclestokens::colorspace, pxr::SdfValueTypeNames->String)
-            .Set(std::string(identifier));
+        if (strlen(ima->colorspace_settings.name) > 0) {
+          shader.CreateInput(cyclestokens::colorspace, pxr::SdfValueTypeNames->String)
+              .Set(std::string(ima->colorspace_settings.name));
+        }
       }
 
       break;
