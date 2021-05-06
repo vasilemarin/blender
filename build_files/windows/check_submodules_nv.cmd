cffe862d01e179b2a794e2b1d6fe9b39ea047a66
makowalski <makowalski@nvidia.com>
2021-05-05 23:46:32 -0400

    New check_submodules_nv.cmd script.
    Modified version of check_submodules.cmd that
    checks out and pulls NVIDIA's branch of addons_contrib.

---
+++ build_files/windows/check_submodules_nv.cmd
@@ -0,0 +1,26 @@
+if NOT exist "%BLENDER_DIR%\source\tools\.git" (
+    echo Checking out sub-modules
+    if not "%GIT%" == "" (
+        "%GIT%" submodule update --init --recursive --progress
+        if errorlevel 1 goto FAIL
+        "%GIT%" submodule foreach git checkout master
+        if errorlevel 1 goto FAIL
+        "%GIT%" submodule foreach git pull --rebase origin master
+        if errorlevel 1 goto FAIL
+        cd "%BLENDER_DIR%\release\scripts\addons_contrib"
+        if errorlevel 1 goto FAIL
+        "%GIT%" checkout umm_add_on
+        if errorlevel 1 goto FAIL
+        "%GIT%" pull --rebase origin umm_add_on
+        if errorlevel 1 goto FAIL
+        goto EOF
+    ) else (
+        echo Blender submodules not found, and git not found in path to retrieve them.
+        goto FAIL
+    )
+)
+goto EOF
+
+:FAIL
+exit /b 1
+:EOF
--- make.bat
+++ make.bat
@@ -68,10 +68,7 @@ if "%TEST%" == "1" (
 	goto EOF
 )
 
-call "%BLENDER_DIR%\build_files\windows\check_submodules.cmd"
-if errorlevel 1 goto EOF
-
-call "%BLENDER_DIR%\build_files\windows\update_nv_addons.cmd"
+call "%BLENDER_DIR%\build_files\windows\check_submodules_nv.cmd"
 if errorlevel 1 goto EOF
 
 if "%BUILD_WITH_NINJA%" == "" (
