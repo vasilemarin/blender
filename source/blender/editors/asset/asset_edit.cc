897ed45675106bb5c2742df8bdaf0574d0b70559
Julian Eisel <julian@blender.org>
2021-06-14 20:22:36 +0200

    Cleanup: Move temporary ID consumer class for assets into own file
    Doesn't really make sense to keep this in `asset_edit.cc`.

--- source/blender/editors/asset/asset_edit.cc
+++ source/blender/editors/asset/asset_edit.cc
@@ -24,19 +24,12 @@
 #include "BKE_asset.h"
 #include "BKE_context.h"
 #include "BKE_lib_id.h"
-#include "BKE_report.h"
-
-#include "BLI_utility_mixins.hh"
 
 #include "BLO_readfile.h"
 
 #include "DNA_ID.h"
 #include "DNA_asset_types.h"
 #include "DNA_space_types.h"
-
-#include "ED_fileselect.h"
-
-#include "MEM_guardedalloc.h"
 
 #include "UI_interface_icons.h"
 
@@ -159,80 +152,4 @@ void ED_asset_handle_get_full_library_path(const bContext *C,
   }
 
   BLO_library_path_explode(asset_path.c_str(), r_full_lib_path, nullptr, nullptr);
-}
-
-class AssetTemporaryIDConsumer : NonCopyable, NonMovable {
-  const AssetHandle &handle_;
-  TempLibraryContext *temp_lib_context_ = nullptr;
-
- public:
-  AssetTemporaryIDConsumer(const AssetHandle &handle) : handle_(handle)
-  {
-  }
-  ~AssetTemporaryIDConsumer()
-  {
-    if (temp_lib_context_) {
-      BLO_library_temp_free(temp_lib_context_);
-    }
-  }
-
-  ID *get_local_id()
-  {
-    return ED_assetlist_asset_local_id_get(&handle_);
-  }
-
-  ID *import_id(const bContext *C,
-                const AssetLibraryReference &asset_library,
-                ID_Type id_type,
-                Main &bmain,
-                ReportList &reports)
-  {
-    const char *asset_name = ED_asset_handle_get_name(&handle_);
-    char blend_file_path[FILE_MAX_LIBEXTRA];
-    ED_asset_handle_get_full_library_path(C, &asset_library, &handle_, blend_file_path);
-
-    temp_lib_context_ = BLO_library_temp_load_id(
-        &bmain, blend_file_path, id_type, asset_name, &reports);
-
-    if (temp_lib_context_ == nullptr || temp_lib_context_->temp_id == nullptr) {
-      BKE_reportf(&reports, RPT_ERROR, "Unable to load %s from %s", asset_name, blend_file_path);
-      return nullptr;
-    }
-
-    BLI_assert(GS(temp_lib_context_->temp_id->name) == id_type);
-    return temp_lib_context_->temp_id;
-  }
-};
-
-AssetTempIDConsumer *ED_asset_temp_id_consumer_create(const AssetHandle *handle)
-{
-  if (!handle) {
-    return nullptr;
-  }
-  BLI_assert(handle->file_data->asset_data != nullptr);
-  return reinterpret_cast<AssetTempIDConsumer *>(
-      OBJECT_GUARDED_NEW(AssetTemporaryIDConsumer, *handle));
-}
-
-void ED_asset_temp_id_consumer_free(AssetTempIDConsumer **consumer)
-{
-  OBJECT_GUARDED_SAFE_DELETE(*consumer, AssetTemporaryIDConsumer);
-}
-
-ID *ED_asset_temp_id_consumer_ensure_local_id(AssetTempIDConsumer *consumer_,
-                                              const bContext *C,
-                                              const AssetLibraryReference *asset_library,
-                                              ID_Type id_type,
-                                              Main *bmain,
-                                              ReportList *reports)
-{
-  if (!(consumer_ && asset_library && bmain && reports)) {
-    return nullptr;
-  }
-  AssetTemporaryIDConsumer *consumer = reinterpret_cast<AssetTemporaryIDConsumer *>(consumer_);
-
-  if (ID *local_id = consumer->get_local_id()) {
-    return local_id;
-  }
-  return consumer->import_id(C, *asset_library, id_type, *bmain, *reports);
 }
