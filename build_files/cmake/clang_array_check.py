b532aff002a33fb0543a67e47099218649880011
Campbell Barton <campbell@blender.org>
2022-02-09 18:35:19 +1100

    Migrate to SPDX license headers
    Using https://gitlab.com/ideasman42/blender_license_spdx_migrate

--- build_files/cmake/clang_array_check.py
+++ build_files/cmake/clang_array_check.py
@@ -1,5 +1,5 @@
 # ---
-# * Licensed under the Apache License, Version 2.0 (the "License");
+# SPDX-License-Identifier: Apache-2.0
 # * you may not use this file except in compliance with the License.
 # * You may obtain a copy of the License at
 # *
